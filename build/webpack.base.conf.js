fe207c15fc200ba6cdece44297b72ce16f7b6f5b
Amir Shahbandegan <am.shahbandegan@gmail.com>
2018-05-17 12:18:02 +0430

    calendar is now jalali

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const webpack = require('webpack')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -21,11 +22,20 @@ module.exports = {
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery",
+      moment: "moment-jalaali",
+    })
+  ],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'fullcalendar$': resolve('src/fullcalendar.js'),
     }
   },
   module: {
