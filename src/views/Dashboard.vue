6c2ecb87813a6af6c51748c865cd4a32a4322b13
Erfan Ranjnar <erfan15000@gmail,com>
2018-07-21 12:25:22 +0430

    wtf?

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -4,15 +4,15 @@
       <b-col sm="6" lg="3">
         <b-card no-body class="bg-primary">
           <b-card-body class="pb-0">
-            <!--<b-dropdown class="float-right" variant="transparent p-0" right>-->
-              <!--<template slot="button-content">-->
-                <!--<i class="icon-settings"></i>-->
-              <!--</template>-->
-              <!--<b-dropdown-item>Action</b-dropdown-item>-->
-              <!--<b-dropdown-item>Another action</b-dropdown-item>-->
-              <!--<b-dropdown-item>Something else here...</b-dropdown-item>-->
-              <!--<b-dropdown-item disabled>Disabled action</b-dropdown-item>-->
-            <!--</b-dropdown>-->
+            <b-dropdown class="float-right" variant="transparent p-0" right>
+              <template slot="button-content">
+                <i class="icon-settings"></i>
+              </template>
+              <b-dropdown-item>Action</b-dropdown-item>
+              <b-dropdown-item>Another action</b-dropdown-item>
+              <b-dropdown-item>Something else here...</b-dropdown-item>
+              <b-dropdown-item disabled>Disabled action</b-dropdown-item>
+            </b-dropdown>
             <h4 class="mb-0">{{last_month_bookings_count}}</h4>
             <p>تعداد رزرو های ماه گذشته</p>
           </b-card-body>
